0330f5882d692d89aeb22d8ba60452eac87b6ce5
Academy <sanela26@gmail.com>
2018-02-20 19:33:13 +0100

    seeder

--- app/User.php
+++ app/User.php
@@ -8,14 +8,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+    public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name','last_name','email', 'password',
     ];
 
     /**
